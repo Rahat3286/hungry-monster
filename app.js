39726b213165111070445c7be3726677048de6eb
Ra- hat <rahat3286@gmail.com>
2021-05-18 11:29:00 +0600

    make some changes

--- app.js
+++ app.js
@@ -13,21 +13,38 @@ const searchFoods = () => {
 const displayfoods = foods => {
     const foodContainer = document.getElementById('food-container');
     foodContainer.innerHTML = '';
-    if(foods){
+    if (foods) {
         foods.forEach(food => {
             const foodDiv = document.createElement('div');
             foodDiv.className = 'd-flex justify-content-center mb-4 col-lg-3';
             foodDiv.innerHTML = `
-                    <div class="card text-center food-cards">
+                    <div class="card text-center food-cards" data-bs-toggle="modal" data-bs-target="#exampleModal">
                         <img src="${food.strMealThumb}" class="card-img-top img-fluid" alt="Food Image">
                         <div class="card-body">
                             <p class="card-title text-center">${food.strMeal}</p>
+                        </div>
+                    </div>
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                ...
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                </div>
+                            </div>
                         </div>
                     </div>      
             `;
             foodContainer.appendChild(foodDiv);
         })
-    }else{
+    } else {
         displayError(false);
     }
     toggleSpinner(false);
@@ -62,3 +79,11 @@ input.addEventListener("keypress", function (event) {
         document.getElementById("searchFoodButton").click();
     }
 });
+
+// create a modal
+const mealIngredientModal = () => {
+    food = food[0];
+    let html = `
+    
+    `
+}
